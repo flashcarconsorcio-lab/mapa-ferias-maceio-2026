cc45dcafb259e0206ffd0e10211e7386c59b0131
flashcarconsorcio-lab <flashcarconsorcio@gmail.com>
2026-09-11 22:42:42 -0300

    Update midias.js

--- data/midias.js
+++ data/midias.js
@@ -6,7 +6,9 @@
       "media/casa/fotos/WhatsApp Image 2026-09-01 at 17.26.24.jpeg",
       "media/casa/fotos/IMG_20181006_191349778.jpg"
     ],
-    "videos":[]
+    "videos":[
+  "media/casa/videos/video-casa-01.mp4.mp4"
+]
   },
   "congonhas":{"fotos":[],"videos":[]},
   "uber":{"fotos":[],"videos":[]},
