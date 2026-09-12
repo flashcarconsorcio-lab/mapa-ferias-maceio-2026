ff5146dbbbb7cec98663b5b6e8efefac9267ab65
flashcarconsorcio-lab <flashcarconsorcio@gmail.com>
2026-09-12 11:17:07 -0300

    Update midias.js

--- data/midias.js
+++ data/midias.js
@@ -1,15 +1,9 @@
 ﻿window.MIDIAS = {
   "tiete":{"fotos":[],"videos":[]},
   "casa":{
-    "fotos":[
-      "media/casa/fotos/WhatsApp Image 2026-09-01 at 17.22.07.jpeg",
-      "media/casa/fotos/WhatsApp Image 2026-09-01 at 17.26.24.jpeg",
-      "media/casa/fotos/IMG_20181006_191349778.jpg"
-    ],
-    "videos":[
-  "media/casa/videos/video-casa-01.mp4.mp4"
-]
-  },
+  "fotos":[],
+  "videos":[]
+},
   "congonhas":{"fotos":[],"videos":[]},
   "uber":{"fotos":[],"videos":[]},
   "maceio":{"fotos":[],"videos":[]}
