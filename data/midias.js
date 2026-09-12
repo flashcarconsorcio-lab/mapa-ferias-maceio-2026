327b93650fc524c005272eb24cb3d53c1790ca93
flashcarconsorcio-lab <flashcarconsorcio@gmail.com>
2026-09-11 21:12:26 -0300

    Update midias.js

--- data/midias.js
+++ data/midias.js
@@ -3,7 +3,8 @@
   "casa":{
     "fotos":[
       "media/casa/fotos/WhatsApp Image 2026-09-01 at 17.22.07.jpeg",
-      "media/casa/fotos/WhatsApp Image 2026-09-01 at 17.26.24.jpeg"
+      "media/casa/fotos/WhatsApp Image 2026-09-01 at 17.26.24.jpeg",
+      "media/casa/fotos/IMG_20181006_191349778.jpg"
     ],
     "videos":[]
   },
